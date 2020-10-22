d29a63dd8dcaae10d19beb54ce7ded09d4931723
kathiavf16 <kathia.vargasfeliz@gmail.com>
2020-10-22 09:43:23 -0400

    testing react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+let style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={style}>
+    <h1>Hello World!</h1>
+    <p>We're glad you're here</p>
+  </div>
+  ,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
