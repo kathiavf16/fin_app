592f8936f7b184cd2ef0b586c62fc9d713631aae
kathiavf16 <kathia.vargasfeliz@gmail.com>
2020-10-22 10:06:05 -0400

    Updates

---
+++ precache-manifest.eca91154a4e5a59c99f3b756cd54cfa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa07f448cbf742a54b73263686c5e60",
+    "url": "/fin_app/index.html"
+  },
+  {
+    "revision": "a92e8110f8ba7f9cbb16",
+    "url": "/fin_app/static/js/2.64d2024a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/fin_app/static/js/2.64d2024a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0238a35917c3445628",
+    "url": "/fin_app/static/js/main.c6ed4aa0.chunk.js"
+  },
+  {
+    "revision": "32aa3d44e0f10156c329",
+    "url": "/fin_app/static/js/runtime-main.dac69795.js"
+  }
+]);
