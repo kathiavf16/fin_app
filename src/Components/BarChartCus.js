203915aa5cbb8c658a745c663369583894015c2f
kathiavf16 <kathia.vargasfeliz@gmail.com>
2020-12-10 12:19:20 -0500

    latest

--- src/Components/BarChartCus.js
+++ src/Components/BarChartCus.js
@@ -18,8 +18,8 @@ export default function BarChartCus({data, key1, key2, xAxis, yAxis}) {
                   >
 
   <CartesianGrid strokeDasharray="3 3" />
-  <XAxis type="number" dataKey={xAxis} />
-  <YAxis type="category" dataKey={yAxis}/>
+  <XAxis type="number" dataKey={xAxis} tick={false} />
+  <YAxis type="category" dataKey={yAxis} />
   <Tooltip formatter={(value) => new Intl.NumberFormat('en').format(value)} />
   <Legend />
   <Bar dataKey={key1} fill="#C34A36" tickFormatter={(value) => new Intl.NumberFormat('en').format(value)}/>
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -11,18 +11,18 @@ export default function Section({ content2, placeholder, regionselection, other,
 
         <div className="section-content" id={id}>
         <h1>{title}</h1>
-        <br></br><br></br>
+        <br></br>
         <div>{text}</div>
-        <br></br><br></br>
+        <br></br>
         <div className="subcontent3" id={id}>{subcontent3}</div>
         <div className="other" id={id}>{other}</div>
         <div className="other2" id={id}>{other2}</div>
-        <br></br><br></br><br></br>
+        <br></br><br></br>
         <div className="placeholder" id={id}>{placeholder}</div>
         <div className="content" id={id}><p>{}</p>{content}</div>
         <br></br><br></br>
         <div className="content2" id={id}><p>{titlecontent}</p>{content2}</div>
-        
+
         <div className="subcontent" id={id}><p className="pa">{titlesubcontent}</p>{subcontent}</div>
         <div className="subcontent2" id={id}><p className="pa">{titlesubcontent2}</p>{subcontent2}</div>
       </div>
